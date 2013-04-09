762c731a74d09da79524975d6b99413070a953a6
Lucas Berezy <lberezy@users.noreply.github.com>
2013-04-09 13:12:46 +0000

    No log message supplied.

--- Project 1/lberezy-tests.py
+++ Project 1/lberezy-tests.py
@@ -1,6 +1,8 @@
 test_cases = {"kinda_fermat":
                   [('submission.kinda_fermat(1,2,3)',False),
-                   ('submission.kinda_fermat(3,4,5)',2)],
+                   ('submission.kinda_fermat(3,4,5)',2),
+                   ('submission.kinda_fermat(4,3,5)',2),
+                   ('submission.kinda_fermat(5,12,13)',2)],
               "unique_long_words":
                   [('submission.unique_long_words("The quick brown fox jumps over the lazy dog.",3)',8),
                    ('submission.unique_long_words("How much wood could a wood chuck chuck.",2)',5)],
--- Project 1/lberezy.py
+++ Project 1/lberezy.py
@@ -3,3 +3,15 @@ def kinda_fermat(a,b,c):
         if(a**n+b**n==c**n):
             return n
     return False
+    
+def unique_long_words(text,wordlen):
+    '''Given a string and wordlen, returns the count of unique words in
+    string that are greater than or equal to wordlen.'''
+    #makes each word in text lowercase then stores each unique word as an
+    #element of a set.
+    words = set(word.lower() for word in (text.split()))
+    count = 0
+    for i in words:
+        if(len(i)>=wordlen):
+            count+=1
+    return counteturn count
