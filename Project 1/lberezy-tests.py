a6d77f0fe13031dad6b8c4f39a13ecc3aadead31
Lucas Berezy <lberezy@users.noreply.github.com>
2013-04-15 12:05:44 +0000

    First draft - all tests passed, need to write own tests.

--- Project 1/lberezy-tests.py
+++ Project 1/lberezy-tests.py
@@ -2,10 +2,11 @@ test_cases = {"kinda_fermat":
                   [('submission.kinda_fermat(1,2,3)',False),
                    ('submission.kinda_fermat(3,4,5)',2),
                    ('submission.kinda_fermat(4,3,5)',2),
-                   ('submission.kinda_fermat(5,12,13)',2)],
+                   ('submission.kinda_fermat(5,12,13)',2)], # 4 of your own tests, none provided
               "unique_long_words":
                   [('submission.unique_long_words("The quick brown fox jumps over the lazy dog.",3)',8),
-                   ('submission.unique_long_words("How much wood could a wood chuck chuck.",2)',5)],
+                   ('submission.unique_long_words("How much wood could a wood chuck chuck.",2)',5),
+                   ('submission.unique_long_words("""Punctuation... Test ... ? 12 3""",2)',3)],
               "symmetric_words":
                   [('submission.symmetric_words("boy dog bevy bully")',["bevy","boy"]),
                    ('submission.symmetric_words("Neither a borrower nor a lender be;")',['a'])],
--- Project 1/lberezy.py
+++ Project 1/lberezy.py
@@ -1,60 +1,91 @@
 def kinda_fermat(a,b,c):
-    """Attempts to validate (non-exhaustively) Fermat's last theorem...kinda_
+    """Attempts to validate (non-exhaustively) Fermat's last theorem...kinda.
+
     Trys exponents from 2 to 10 for Fermat equality given a,b,c. Returns the
-    exponent if the equality holds, otherwise returns False."""
+    (smallest) exponent if the equality holds, otherwise returns False.
+    """
 
     for n in range (2,11):
         if(a**n+b**n==c**n):
-            return n        #function returns n if equality holds (smallest n) 
+            return n        # function returns n if equality holds (smallest n) 
     return False
 
 def strip_punc(word):
-    """Function used to strip given set of punctuation from right of word"""
+    """Strips basic punctuation from right of string."""
 
-    punc = """,.:;!?'"-"""      #punctuation to strip (as per requirement)
-    return word.rstrip(punc)    #strip punc from right until alphabet character
+    punc = """,.:;!?'"-"""      # punctuation to strip (as per requirement)
+    return word.rstrip(punc)    # strip punc from right until alpha character
 
     
 def unique_long_words(text,wordlen):
-    """Returns the count of unique words in
-    string that are greater than or equal to wordlen."""
+    """Counts words in text that are greater than or equal to wordlen."""
 
-    #makes each word in text lowercase and strips punctuation
-    #then stores each unique word as a unique element of a set.
+    # makes each word in text lowercase and strips punctuation
+    # then stores each unique word as a unique element of a set.
     words = set(strip_punc(word.lower()) for word in (text.split()))
     count = 0
-    for i in words:             #check each word
-        if(len(i)>=wordlen):    #if >= wordlen, increment total count
+    for i in words:             # check each word
+        if(len(i)>=wordlen):    # if >= wordlen, increment total count
             count+=1
     return count
 
-def is_symmetric(word):
-    """returs word if word is symmetric else returns false"""
-    for i in range(len(word)/2+1):
-        #if(not(word[i]==alphabet[i] && word[::-])): #extend this function
+
 
 def symmetric_words(text):
-    """Returns sorted list of symmetric words from given text. A symmetric
-    word is one in which the outermost letter pairs are equidistant across
-    the alphabet."""
+    """Returns sorted list of symmetric words from given text.
 
-    #makes each word in text lowercase and strips punctuation
-    #then stores each unique word as a unique element of a set.
-    from string import ascii_lowercase #why not
-    alphabet = ascii_lowercase
+    A symmetric word is one in which the outermost letter pairs are 
+    equidistant across the alphabet.
+    """
+
+
+    def is_symmetric(word):
+        """returns word if word is symmetric else returns false"""
+        from string import ascii_lowercase as myalphabet # grab ascii alphabet
+        word_reverse = word[::-1]
+        for i in range(0,len(word)/2):
+            # check
+            if(not(myalphabet.find(word[i])+1) \
+             == (len(myalphabet) - myalphabet.find(word_reverse[i]))):
+                return False
+        return word
+
+    # makes each word in text lowercase and strips punctuation
+    # then stores each unique word as a unique element of a set.
+    words_symmetric = []
     words = set(strip_punc(word.lower()) for word in (text.split()))
+    if words == []:
+        return []
     for word in words:
-        for i in range(len(word)/2+1):
-            if(not(word[i]==alphabet[i] && word[::-]))
-
-    return
+        if(is_symmetric(word)):
+            words_symmetric.append(word)
+    words_symmetric.sort()
+    return words_symmetric
 
 
 def least_vowel_words(text):
     """Returns sorted list whose elements satisfy the condition of having
-    the lowest vowel proportion of all the words in the given text."""
+    the lowest vowel proportion of all the words in the given text.
 
-    #makes each word in text lowercase and strips punctuation
-    #then stores each unique word as a unique element of a set.
-    words = set(strip_punc(word.lower()) for word in (text.split()))
-    return
+    The algorithm used is not computationally efficient and can be improved,
+    but computing power is cheap.
+    """
+
+
+    def vowel_ratio(word):
+        '''Calculates vowel ('aeiou') ratio of string as float.'''
+        vowels = 'aeiou'
+        return sum(word.count(x) for x in vowels)/float(len(word))
+
+    # makes each word in text lowercase and strips punctuation
+    # then stores each unique word as a unique element of a set.
+    words = [strip_punc(word.lower()) for word in text.split()]
+    #words = [word for word in words if word != '']
+    words = filter(None,words) # filter out null strings
+    if words == []:
+        return []
+    min_ratio = min([vowel_ratio(word) for word in words])
+    result = \
+    [word for word in words if vowel_ratio(word) == min_ratio]
+    result.sort()
+    return result
